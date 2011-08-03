558117b9c91f1957a5e9d3de5b24b20baef00c41
Nick Reid <nickreid@nickreid.com>
2011-08-03 12:20:00 -0700

    added more app structure

--- permits.py
+++ permits.py
@@ -1,11 +1,21 @@
 from flask import Flask, request, session, g, redirect, url_for, \
 	abort, render_template, flash
 from flaskext.sqlalchemy import SQLAlchemy
+from flaskext.uploads import *
 
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/test.db'
 db = SQLAlchemy(app)
+
+spreadsheets = UploadSet('spreadsheet',DATA)
+
+@app.route('/', methods=['GET','POST'])
+def upload_excel():
+	if request.method == 'POST':
+		return 'POST REQUEST'
+	return render_template('upload.html')
+
 
 class Permit(db.Model):
 	id = db.Column(db.Integer, primary_key=True)
@@ -14,31 +24,31 @@ class Permit(db.Model):
 	status_date = db.Column(db.Integer)
 	expiration_date = db.Column(db.Integer)
 	issued = db.Column(db.Boolean)
-	
+
 	address_id = db.Column(db.Integer, db.ForeignKey('address.id'))
 	address = db.relationship('Address', backref=db.backref('permits', lazy='dynamic'))
-	
+
 	def __init__(self, app_id, file_date, status_date, expiration_date, issued):
 		self.app_id = app_id
 		self.file_date = file_date
 		self.status_date = status_date
 		self.expiration_date = expiration_date
 		self.issued = issued
-		
-		
+
+
 	def __repr__(self):
 		return '<Permit %r>' % self.app_id
-		
+
 
 class Address(db.Model):
 	id = db.Column(db.Integer, primary_key=True)
 	street = db.Column(db.String(500))
-	
+
 	def __init__(self,street):
 		self.street = street
-		
+
 	def __repr__(self):
 		return '<Address %r>' % self.street
 
 
-
+app.run(debug=True)
