7abed4ec90ae5267cb0a8529589d376506d2f27d
Nick Reid <nickreid@nickreid.com>
2011-08-04 13:08:39 -0700

    added save routine for addresses and permits

--- permits.py
+++ permits.py
@@ -68,8 +68,11 @@ def make_unix_from_string(letters):
 def add_permit(data):
 	# find or make address
 #	if Permit.query.filter_by(app_id=data['app_id']).first() is None:
-	if 'app_id' in data and 'status_date' in data and 'file_date' in data and 'expiration_date' in data:
+	if 'app_id' in data and 'status_date' in data and 'file_date' in data and 'expiration_date' in data and 'street_number' in data and 'street_name' in data and 'street_suffix' in data:
+		address = Address(data['street_number'],data['street_name']+" "+data['street_suffix'])
 		permit = Permit(data['app_id'],data['file_date'],data['status_date'],data['expiration_date'])
+		permit.address = address
+		db.session.add(address)
 		db.session.add(permit)
 		db.session.commit()
 		return True
@@ -82,8 +85,8 @@ class Permit(db.Model):
 	status_date = db.Column(db.Integer)
 	expiration_date = db.Column(db.Integer)
 
-#	address_id = db.Column(db.Integer, db.ForeignKey('address.id'))
-#	address = db.relationship('Address', backref=db.backref('permits', lazy='dynamic'))
+	address_id = db.Column(db.Integer, db.ForeignKey('address.id'))
+	address = db.relationship('Address', backref=db.backref('permits', lazy='dynamic'))
 
 	def __init__(self, app_id, file_date, status_date, expiration_date):
 		self.app_id = app_id
