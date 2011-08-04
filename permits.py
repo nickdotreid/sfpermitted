768b0db25af6853b7f9e4b6ccf478438bf69ca52
Nick Reid <nickreid@nickreid.com>
2011-08-04 12:28:43 -0700

    added status date to permits

--- permits.py
+++ permits.py
@@ -3,6 +3,8 @@ from flask import Flask, request, session, g, redirect, url_for, \
 from werkzeug import secure_filename
 from flaskext.sqlalchemy import SQLAlchemy
 
+from models import Permit
+
 import csv
 import time
 import re
@@ -52,6 +54,8 @@ def permit_row_clean(row):
 				clean['file_date'] = make_unix_from_string(row[key])
 			if re.search('EXPIRATION',key):
 				clean['expiration_date'] = make_unix_from_string(row[key])
+			if re.search('STATUS_DATE',key):
+				clean['status_date'] = make_unix_from_string(row[key])
 	return clean
 
 def make_unix_from_string(letters):
@@ -60,7 +64,7 @@ def make_unix_from_string(letters):
 def add_permit(data):
 	# find or make address
 #	if Permit.query.filter_by(app_id=data['app_id']).first() is None:
-	if 'app_id' in data and 'file_date' in data and 'expiration_date' in data:
+	if 'app_id' in data and 'status_date' in data and 'file_date' in data and 'expiration_date' in data:
 		permit = Permit(data['app_id'],data['file_date'],data['expiration_date'])
 		db.session.add(permit)
 		db.session.commit()
@@ -71,14 +75,16 @@ class Permit(db.Model):
 	id = db.Column(db.Integer, primary_key=True)
 	app_id = db.Column(db.String(80))
 	file_date = db.Column(db.Integer)
+	status_date = db.Column(db.Integer)
 	expiration_date = db.Column(db.Integer)
 
 #	address_id = db.Column(db.Integer, db.ForeignKey('address.id'))
 #	address = db.relationship('Address', backref=db.backref('permits', lazy='dynamic'))
 
-	def __init__(self, app_id, file_date=0, expiration_date=0):
+	def __init__(self, app_id, file_date, status_date, expiration_date):
 		self.app_id = app_id
 		self.file_date = file_date
+		self.status_date = status_date
 		self.expiration_date = expiration_date
 
 
