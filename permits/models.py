c9fdca7d6f1c671b52a7f212d5fed80c2bc18817
Nick Reid <nickreid@nickreid.com>
2011-08-17 12:04:37 -0700

    moved permit save function to models

--- permits/models.py
+++ permits/models.py
@@ -1,5 +1,18 @@
 from permits import db
 
+def add_permit(data):
+	# find or make address
+#	if Permit.query.filter_by(app_id=data['app_id']).first() is None:
+	if 'app_id' in data and 'status_date' in data and 'file_date' in data and 'expiration_date' in data and 'street_number' in data and 'street_name' in data and 'street_suffix' in data:
+		address = Address(data['street_number'],data['street_name']+" "+data['street_suffix'])
+		permit = Permit(data['app_id'],data['file_date'],data['status_date'],data['expiration_date'])
+		permit.address = address
+		db.session.add(address)
+		db.session.add(permit)
+		db.session.commit()
+		return True
+	return False
+
 class Permit(db.Model):
 	id = db.Column(db.Integer, primary_key=True)
 	app_id = db.Column(db.String(80))
--- permits/views.py
+++ permits/views.py
@@ -28,16 +28,3 @@ def upload_spreadsheet():
 			flash("Parsing "+ filename)
 			parse_csv(UPLOAD_PATH + filename)
 	return render_template('upload.html')
-
-def add_permit(data):
-	# find or make address
-#	if Permit.query.filter_by(app_id=data['app_id']).first() is None:
-	if 'app_id' in data and 'status_date' in data and 'file_date' in data and 'expiration_date' in data and 'street_number' in data and 'street_name' in data and 'street_suffix' in data:
-		address = Address(data['street_number'],data['street_name']+" "+data['street_suffix'])
-		permit = Permit(data['app_id'],data['file_date'],data['status_date'],data['expiration_date'])
-		permit.address = address
-		db.session.add(address)
-		db.session.add(permit)
-		db.session.commit()
-		return True
-	return False
