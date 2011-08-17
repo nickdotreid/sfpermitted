3bd2fe7baa7689ce992d78a911c2a5119a9aa255
Nick Reid <nickreid@nickreid.com>
2011-08-17 12:02:33 -0700

    seperated parsing files

--- permits/views.py
+++ permits/views.py
@@ -27,39 +27,6 @@ def upload_spreadsheet():
 			flash("Parsing "+ filename)
 			parse_csv(UPLOAD_PATH + filename)
 	return render_template('upload.html')
-	
-def parse_csv(file_location):
-	data = csv.DictReader(open(file_location))
-	count = 0
-	for row in data:
-		row = permit_row_clean(row)
-		if add_permit(row):
-			count+=1
-	if count>0:
-		flash("Added " + str(count) + "permits")
-		
-def permit_row_clean(row):
-	clean = {}
-	for key in row.keys():
-		if row[key]:
-			if re.search('(APP)',key):
-				clean['app_id'] = row[key].lstrip('# ')
-			if re.search('FILE',key):
-				clean['file_date'] = make_unix_from_string(row[key])
-			if re.search('EXPIRATION',key):
-				clean['expiration_date'] = make_unix_from_string(row[key])
-			if re.search('STATUS_DATE',key):
-				clean['status_date'] = make_unix_from_string(row[key])
-			if re.search('STREET_NUMBER',key):
-				clean['street_number'] = row[key]
-			if re.search('AVS_STREET_NAME',key):
-				clean['street_name'] = row[key]
-			if re.search('AVS_STREET_SFX',key):
-				clean['street_suffix'] = row[key]
-	return clean
-
-def make_unix_from_string(letters):
-	return int(mktime(time.strptime(letters,'%d-%b-%y')))
 
 def add_permit(data):
 	# find or make address
