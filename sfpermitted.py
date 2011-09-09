df03e66ba537c5910494c473c183ee6419c3ff80
Nick Reid <nickreid@nickreid.com>
2011-09-09 10:45:43 -0700

    added mapper to app paths

--- sfpermitted.py
+++ sfpermitted.py
@@ -1,8 +1,10 @@
 from flask import Flask
 from permits.views import permit_app
+from mapper.views import mapper_app
 
 app = Flask(__name__)
 #app.config.from_object('sfpermitted.settings') #not sure where this should come from
 app.config.from_envvar('YOURAPPLICATION_SETTINGS')
 
+app.register_blueprint(mapper_app)
 app.register_blueprint(permit_app,url_prefix='/permits')
