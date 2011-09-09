4db034b6f5103459a37ca10462ed80a6219a844a
Nick Reid <nickreid@nickreid.com>
2011-09-09 10:43:03 -0700

    making blueprint for mapper pages

---
+++ mapper/views.py
@@ -0,0 +1,8 @@
+from flask import Blueprint, request, session, g, redirect, url_for, \
+	abort, render_template, flash
+
+mapper_app = Blueprint('mapper_app', __name__,template_folder='templates')
+
+@permit_app.route('/')
+def index():
+	return render_template('map.html')
